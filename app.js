7103cf30348dd81278223a9132f8b899b4ab3f60
Avielle Lewis <aviselanj@gmail.com>
2024-01-16 22:54:17 -0600

    added api functioning to app

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 "use strict"
+const searchDropMenu = document.getElementById("search-dropdown");
 
 $("#heading").append("<h1>Discover Amiibo</h1>");
 const content =
-    $.get("https://amiiboapi.com/api/amiibo/?showusage&showgames", function (data) {
+    $.get("https://amiiboapi.com/api/amiibo/?type=figure", function (data) {
 
         for (let key in data) {
             const main = data[key];
-            // Console logs all the 
-            // values in the objArr Array:
-            console.log(main[main.length - 1]);
-            console.log(main[0]);
-            console.log(main[0].image);
-            console.log(main.length);
-            let allInputs = $(":input");
-            let formChildren = $("form > *");
+            // Console logs all the values in the main Array:
+           
+            for(let i = 0; i < main.length; i++){
+                console.log(main[i])
+                console.log(main[i].image)
+                console.log(main[i].character)
+                $("form").on("submit", function (event) {
+                    event.preventDefault();
+                    $('#window').append(`<img src=${main[i].image} />`)
+                    $('#window').append(`<p>${main[i].name}</p>`)
+                });
 
-
-            $("form").on("submit", function (event) {
-                event.preventDefault();
-                $('#window').append(`<img src=${main[0].image} />`)
-            });
-        }
-    }); 
+            }
+          
+          
+    }}); 
